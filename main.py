afb8d31fc8968ae6041f89f1302424ed4a7aae5a
bbh <jhe27216@gmail.com>
2025-12-08 22:09:49 +0800

    'complete_workflows'

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello from bbh-test!")
+
+def test():
+    print('='*10)
+    print('test process')
+    print('='*10)
+
+
+if __name__ == "__main__":
+    main()
+    test()
